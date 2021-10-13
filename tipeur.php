a8f1fd8cb473e1b8e7315bba1d3963f0ea72096a
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2021-10-13 20:41:20 +0200

    Tipeur finis

---
+++ tipeur.php
@@ -0,0 +1,24 @@
+<?php
+include 'Fheader.php';
+include 'gestionDB/read/Findex.php';
+$_SESSION['idUniversVisite'] = NULL;
+$_SESSION['nomUnivers'] = NULL;
+$requetteSQL = "SELECT `login` FROM `users` WHERE `consentementUser`= 1 AND `role` = 1 AND `tiper` = 1 ORDER BY `login` ASC";
+$data = $conn->prepare($requetteSQL);
+$data->execute();
+$data->setFetchMode(PDO::FETCH_ASSOC);
+$dataUser = $data->fetchAll();
+ ?>
+<section>
+  <article>
+    <h4>Merci aux tipers de faire vivre de site et participé au frais de mise en ligne</h4>
+    <ul class="listSimple">
+      <?php foreach ($dataUser as $key) {
+        echo '<li><strong class="famous">'.$key['login'].'</strong></li>';
+      } ?>
+    </ul>
+  </article>
+</section>
+ <?php
+   include 'footer.php';
+ ?>
